d52f89a111543fb3241d2e1c4678d61536d7ccaa
Sridhar Subramanian <sridhar.subramanian@cardio.chboston.org>
2017-08-17 15:51:18 -0400

    github test
    github test

---
+++ test2/test2/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace test2
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
